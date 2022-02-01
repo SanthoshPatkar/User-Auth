f7046e10c840d68c974afb68cc7177963bb5fa6d
SanthoshPatkar <ssanthoshp159@gmail.com>
2022-02-01 18:19:34 +0530

    database creation

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const express=require('express')
+const dotenv=require('dotenv');
+const express=require('express');
 const app=express()
+
+dotenv.config({path: './config.env'});
+require('./db/conn')
 
 app.get('/',(req,res)=>{
     res.send("hello sir")
@@ -8,5 +12,5 @@ app.get('/about',(req,res)=>{
     res.send("hello sir this is about me")
 })
 app.listen(3001,()=>{
-    console.log("srver is running at port 3001")
+    console.log("server is running at port 3001")
 })
